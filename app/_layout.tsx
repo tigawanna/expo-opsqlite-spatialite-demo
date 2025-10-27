1a1c42b4cac6f1f253ff0376e5b242fb019fab67
tigawanna <denniskinuthiaw@gmail.com>
2025-10-27 13:22:38 +0300

    add ts query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,23 +5,34 @@ import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { InitDatabase } from "@/lib/drizzle/InitDatabase";
+import {
+  useOnlineManager,
+  useAppState,
+  onAppStateChange,
+} from "@/lib/tanstack/query/react-native-setup-hooks";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/tanstack/query/client";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
+  useOnlineManager();
+  useAppState(onAppStateChange);
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <InitDatabase>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
-        </Stack>
-        <StatusBar style="auto" />
-      </InitDatabase>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <InitDatabase>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
+          </Stack>
+          <StatusBar style="auto" />
+        </InitDatabase>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
